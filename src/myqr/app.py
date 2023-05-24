70902881524abda79375c4ca5a6f3b983630c315
tct123 <42028373+tct123@users.noreply.github.com>
2023-05-24 17:12:54 +0200

    Update app.py

--- src/myqr/app.py
+++ src/myqr/app.py
@@ -24,10 +24,10 @@ class MyQR(toga.App):
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-        readqr()
-    def createqr():
+        self.readqr(widget)
+    def createqr(self, widget):
         pass
-    def readqr():
+    def readqr(self, widget):
         data = decode(Image.open("resources/image.png"))
         print(data)
 
