7ded8cd927cd554569c8660f7cf31377021a2106
tct123 <42028373+tct123@users.noreply.github.com>
2023-08-28 00:32:44 +0200

    Update read.py

--- read.py
+++ read.py
@@ -3,6 +3,6 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 import cv2
 
-qr = pyqrcode.create("test123")
-qr.png("mycode.png",scale=8)
-data = decode(Image.open("myqrcode.png"))
+# qr = pyqrcode.create("test123")
+# qr.png("mycode.png",scale=8)
+data = decode(Image.open("mycode.png"))
