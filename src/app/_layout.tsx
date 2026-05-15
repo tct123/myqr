bcfe9d551ed9f77bbb8897899ed07123e2f0b589
tct123 <42028373+tct123@users.noreply.github.com>
2026-05-15 05:25:30 +0200

    .

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,23 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useTheme } from '@react-navigation/native';
+import { Icon, Label, VectorIcon } from 'expo-router';
 import { NativeTabs, } from 'expo-router/unstable-native-tabs';
 
 
 export default function RootLayout() {
+  const theme = useTheme();
   return (
-    <NativeTabs>
+    <NativeTabs tintColor={theme.colors.primary} backgroundColor={theme.colors.background}>
       <NativeTabs.Trigger name="index">
-        <NativeTabs.Trigger.Label>Scan</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="qrcode" />}></NativeTabs.Trigger.Icon>
+        <Label>Scan</Label>
+        <Icon src={<VectorIcon family={MaterialCommunityIcons} name="qrcode" />}></Icon>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="generate">
-        <NativeTabs.Trigger.Label>Generate</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="qrcode-edit" />}></NativeTabs.Trigger.Icon>
+        <Label>Generate</Label>
+        <Icon src={
+          <VectorIcon family={MaterialCommunityIcons} name="qrcode-edit" />
+        }>
+        </Icon>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
