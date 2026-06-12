7ce69aa7b1ce718d2735f0fccfea51bef58f5fa0
tct123 <42028373+tct123@users.noreply.github.com>
2026-06-12 15:23:56 +0200

    .

--- functions/loadfile.tsx
+++ functions/loadfile.tsx
@@ -1,6 +1,20 @@
-import { File } from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { Alert } from 'react-native';
 
-export default function loadfile() {
-    const file = File.pickFileAsync({ mimeTypes: ["image/jpeg", "image/png", "image/svg+xml"] })
-    return file
+export default function LoadFile() {
+    const [image, setImage] = useState<string | null>(null);
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library.
+        // Manually request permissions for videos on iOS when `allowsEditing` is set to `false`
+        // and `videoExportPreset` is `'Passthrough'` (the default), ideally before launching the picker
+        // so the app users aren't surprised by a system dialog after picking a video.
+        // See "Invoke permissions for videos" sub section for more details.
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (!permissionResult.granted) {
+            Alert.alert('Permission required', 'Permission to access the media library is required.');
+            return;
+        }
+    }
 }
